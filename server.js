3ab286f57e3234b203fde32a2699ec1a355ce7fa
jtfell <julesfell@gmail.com>
2015-06-18 21:49:03 +1000

    fix port stuff

--- server.js
+++ server.js
@@ -43,4 +43,9 @@ app.get('/', function(req,res) {
 });
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(8080);
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+app.listen(server_port, server_ip_address, function(){
+  console.log("Listening on " + server_ip_address + ", server_port " + server_port)
+});

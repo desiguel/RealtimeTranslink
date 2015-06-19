3be1c303e508ca7bc3fe6b1b8924b8c2bfafb27c
jtfell <julesfell@gmail.com>
2015-06-19 19:47:16 +1000

    fix template ajax3

--- public/app/main.js
+++ public/app/main.js
@@ -1,9 +1,9 @@
 define(function (require) {
-    var routes = require("./routes");
-    var view = require("./view");
+	var routes = require("./routes");
+	var view = require("./view");
 
-    // Plot the new positions of the vehicles on the active route
-    var newPositionCallback = function(newPositions) {
+	// Plot the new positions of the vehicles on the active route
+	var newPositionCallback = function(newPositions) {
 		view.hideLoadingDialog();
 		view.eraseStops();
 
@@ -19,15 +19,15 @@ define(function (require) {
 	}
 
 	// Change the route being shown on the map
-    var activeRouteCallback = function(newRoute) {
-    	routes.getRealTimeData(newRoute, newPositionCallback);
+	var activeRouteCallback = function(newRoute) {
+		routes.getRealTimeData(newRoute, newPositionCallback);
 
-    	// Remove the current route
-    	view.eraseActiveRoute();
-    	view.eraseStops();
-    	view.showLoadingDialog();
+		// Remove the current route
+		view.eraseActiveRoute();
+		view.eraseStops();
+		view.showLoadingDialog();
 
-    	// Render the new route
+		// Render the new route
 		routes.setActive(newRoute);
 		view.renderActiveRoute(routes.getActive());
 		view.renderTable(routes.get(), routes.getActive(), activeRouteCallback, removeRouteCallback);
@@ -35,18 +35,23 @@ define(function (require) {
 
 	// Remove a route from the list on the right
 	var removeRouteCallback = function(oldRoute) {
-    	view.eraseActiveRoute();
+		view.eraseActiveRoute();
 		routes.drop(oldRoute);
 		view.renderActiveRoute(routes.getActive());
 		view.renderTable(routes.get(), routes.getActive(), activeRouteCallback, removeRouteCallback);
 	}
 
 	// Add a route to the list on the right
-    var addRouteCallback = function(route) {
-    	view.eraseActiveRoute();
-    	routes.add(route);
-    	view.renderTable(routes.get(), routes.getActive(), activeRouteCallback, removeRouteCallback);
+	var addRouteCallback = function(route) {
+		view.eraseActiveRoute();
+		routes.add(route);
+		view.renderTable(routes.get(), routes.getActive(), activeRouteCallback, removeRouteCallback);
 	}
+
+	// Need to init the map better
+	//google.maps.event.addDomListener(window, 'load', initialize);
+	view.initialize();
+	view.renderDialogTable(routes.getAllRoutes(), addRouteCallback);
 
 	// Example routes for testing
 	routes.add('209');
@@ -55,15 +60,12 @@ define(function (require) {
 	routes.add('302');
 	routes.add('222');
 
-	// Need to init the map better
-	//google.maps.event.addDomListener(window, 'load', initialize);
-	view.initialize();
-	view.renderDialogTable(routes.getAllRoutes(), addRouteCallback);
+	
 	view.renderTable(routes.get(), routes.getActive(), activeRouteCallback, removeRouteCallback);
 
 	// Update bus positions every 10s
 	var updatePositions = function() {
-    	routes.getRealTimeData(routes.getActive(), newPositionCallback);
-    }
+		routes.getRealTimeData(routes.getActive(), newPositionCallback);
+	}
 	window.setInterval(updatePositions, 10000);
 });
